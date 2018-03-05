80eb1bd57fc656b9dd9f755a999dd437d8b67768
Adrià Biarnés <adriabiarnesbelso@enti.cat>
2018-03-05 15:06:36 +0100

    render.cpp esborrat

--- glframework_starthere/code/my_render_code.cpp
+++ glframework_starthere/code/my_render_code.cpp
@@ -416,7 +416,7 @@ void main() {\n\
 	void drawDollyCubes() {
 		const std::array<glm::vec3, 9> cubePositions = { glm::vec3(-0.0f, 3.5f, 0.0f), glm::vec3(-2.0f, 0.5f, -1.0f), glm::vec3(2.0f, 0.5f, -1.0f),
 			glm::vec3(-7.0f, 1.5f, -4.0f), glm::vec3(7.0f, 1.5f, -4.0f), glm::vec3(6.0f, 1.5f, 7.0f), glm::vec3(-6.0f, 1.5f, 7.0f),
-			glm::vec3(-13.0f, 3.5f, -1.0f), glm::vec3(13.0f, 3.5f, -1.0f), };
+			glm::vec3(-13.0f, 3.5f, 0.0f), glm::vec3(13.0f, 3.5f, 0.0f), };
 		const std::array<glm::vec3, 9> cubeScales = { glm::vec3(2, 2, 2), glm::vec3(1, 1, 1), glm::vec3(1, 1, 1), 
 			glm::vec3(3, 3, 3) , glm::vec3(3, 3, 3), glm::vec3(2.5f, 1, 2), glm::vec3(2.5f, 1, 2),
 			glm::vec3(1, 11, 1), glm::vec3(1, 11, 1) };
